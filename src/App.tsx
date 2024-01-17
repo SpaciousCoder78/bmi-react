4ae26958017f3220325f6236baee367447788801
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-01-17 15:43:18 +0530

    fixed the errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+
 function App() {
-  // const [count, setCount] = useState(0)
+  
 
   return (
     <div>
-    <h1>BMI Calculator</h1>
-    <h3>Calculator</h3>
-    <p>Enter the weight(in kg) and height(in cm) to get the BMI</p>
-    <input id="weight" type="textbox" placeholder="Enter your weight(kg)"/>
-    <br></br>
-    <input id="height" type="textbox" placeholder="Enter your height(cm)"/>
-    <br>
-    </br>
-    <button onClick="{calculator}">Calculate</button>
-    <br></br><br></br>
+      <h1>BMI Calculator</h1>
+      <h3>Calculator</h3>
+      <p>Enter the weight (in kg) and height (in cm) to get the BMI</p>
+      <input id="weight" type="textbox" placeholder="Enter your weight (kg)" />
+      <br />
+      <input id="height" type="textbox" placeholder="Enter your height (cm)" />
+      <br />
+      <button onClick={calculator}>Calculate</button>
+      <br /><br />
     </div>
-  )
+  );
 }
-function calculator(){
-  let weight=document.getElementById("weight").value;
-  let height=document.getElementById("height").value;
-  let metreheight=height*0.01;
-  let metresq=metreheight**2;
-  let bmi=weight/metresq;
-  alert("Your BMI is "+bmi);
+
+function calculator() {
+  const weight = document.getElementById("weight") as HTMLInputElement;
+  const height = document.getElementById("height") as HTMLInputElement;
+  const wt = parseFloat(weight?.value || "0");
+  const hei = parseFloat(height?.value || "0");
+  const metreheight = hei * 0.01;
+  const metresq = metreheight ** 2;
+  const bmi = wt / metresq;
+  alert("Your BMI is " + bmi);
 }
-export default App
+
+export default App;
